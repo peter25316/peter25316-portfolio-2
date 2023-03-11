2461c47e35280b76854cd6dd0eb670aa7f848aab
Peter Huynh <peterlonghaihuynh25@gmail.com>
2023-03-11 18:23:54 -0500

    update

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,5 +1,5 @@
 const Main = (props) => {
-  return <main className="mx-auto p-4 max-w-lg">{props.children}</main>;
+  return <main className="mx-auto p-4 max-w-lg w-full">{props.children}</main>;
 };
 
 export default Main;
--- src/pages/projects/index.jsx
+++ src/pages/projects/index.jsx
@@ -1,11 +1,34 @@
+import Link from "next/link";
+import Image from "next/legacy/image";
 import Layout from "../layout";
 import Section from "@/components/section";
+import sortVisual from "../../../public/sorting-visualization.png";
+import quizzcal from "../../../public/quizzical-landing.png";
 
 const Projects = () => {
   return (
     <Layout title="Projects">
       <Section>
-        <h1>Projects Section</h1>
+        <div>
+          <h3 className="section-title">Works</h3>
+          <div className="flex flex-wrap gap-4">
+            <div className="basis-64 flex-1 md:basis-0 text-center">
+              <Link href="/projects">
+                <Image className="rounded-lg" src={sortVisual} />
+              </Link>
+              <p className="text-xl my-1">Sorting Visualization</p>
+              <p className="text-sm">
+                A web application that visualize sorting visualization
+              </p>
+            </div>
+
+            <div className="basis-64 flex-1 md:basis-0 ">
+              <Link href="/projects">
+                <Image className="rounded-lg" src={quizzcal} />
+              </Link>
+            </div>
+          </div>
+        </div>
       </Section>
     </Layout>
   );
