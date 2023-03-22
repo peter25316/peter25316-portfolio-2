59dd90a51cd7018cee734dec3e28353559a956de
Peter Huynh <peterlonghaihuynh25@gmail.com>
2023-03-22 11:20:43 -0400

    update

--- src/pages/projects/index.jsx
+++ src/pages/projects/index.jsx
@@ -15,54 +15,56 @@ const Projects = () => {
   return (
     <Layout title="Projects">
       <Section>
-        <div>
-          <h3 className="section-title">Works</h3>
-          <div className="grid grid-cols-1 gap-6 md:grid-cols-2">
-            <div className="mb-4 text-center">
-              <Link href="/projects">
-                <Image className="rounded-lg" src={sortVisual} />
-                <p className="text-xl my-1">Sorting Visualization</p>
-                <p className="text-sm">
-                  A web application that visualize sorting visualization
-                </p>
-              </Link>
-            </div>
+        <h3 className="section-title">Works</h3>
+        <div className="grid grid-cols-1 gap-6">
+          <div className="mb-4 text-center">
+            <Link href="/projects">
+              <Image className="rounded-lg" src={sortVisual} />
+              <p className="text-xl my-1">Sorting Visualization</p>
+              <p className="text-sm">
+                A web application that visualize sorting visualization
+              </p>
+            </Link>
+          </div>
 
-            <div className="mb-4 text-center">
-              <Link href="/projects">
-                <Image className="rounded-lg" src={quizzcal} />
-                <p className="text-xl my-1">Quizzical</p>
-                <p className="text-sm">
-                  Quiz WebApp accesses trivia questions from the Open Trivia
-                  Database API.
-                </p>
-              </Link>
-            </div>
+          <div className="mb-4 text-center">
+            <Link href="/projects">
+              <Image className="rounded-lg" src={quizzcal} />
+              <p className="text-xl my-1">Quizzical</p>
+              <p className="text-sm">
+                Quiz WebApp accesses trivia questions from the Open Trivia
+                Database API.
+              </p>
+            </Link>
+          </div>
+        </div>
+      </Section>
 
-            <div className="mb-4 text-center">
-              <Link href="/projects">
-                <Image className="rounded-lg" src={weatherBot} />
-                <p className="text-xl my-1">Weather Bot</p>
-                <p className="text-sm">
-                  A Weather Discord Bot reports the weather around the world
-                  with just few simple commands.
-                </p>
-              </Link>
-            </div>
+      <Section delay={0.1}>
+        <div className="grid grid-cols-1 gap-6">
+          <div className="mb-4 text-center">
+            <Link href="/projects">
+              <Image className="rounded-lg" src={weatherBot} />
+              <p className="text-xl my-1">Weather Bot</p>
+              <p className="text-sm">
+                A Weather Discord Bot reports the weather around the world with
+                just few simple commands.
+              </p>
+            </Link>
+          </div>
 
-            <div className="mb-4 text-center">
-              <Link href="/projects">
-                <Image
-                  className="rounded-lg"
-                  src={dark ? portfolioLight : portfolioDark}
-                />
-                <p className="text-xl my-1">Portfolio</p>
-                <p className="text-sm">
-                  Responsive website built with Next.js and styled with Tailwind
-                  CSS
-                </p>
-              </Link>
-            </div>
+          <div className="mb-4 text-center">
+            <Link href="/projects">
+              <Image
+                className="rounded-lg"
+                src={dark ? portfolioLight : portfolioDark}
+              />
+              <p className="text-xl my-1">Portfolio</p>
+              <p className="text-sm">
+                Responsive website built with Next.js and styled with Tailwind
+                CSS
+              </p>
+            </Link>
           </div>
         </div>
       </Section>
